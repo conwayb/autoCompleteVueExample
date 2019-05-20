a554a6e11359b70f6a4c4b93656f4f3eca787fc3
Brandon Conway <conwayb@pdx.edu>
2019-05-20 16:45:40 -0700

    Add getUrl and selected and clear events

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <h3>Horses and stuff</h3>
-    <p>
-      With Array
-      <auto-complete :dataArray="horses"/>
-    </p>
-    <p>
-      With method
-      <auto-complete :method="getHorses"/>
-    </p>
-    <p>
-      With Url
-      <auto-complete
-        url="https://api.datamuse.com/words?max=10&sp="
-        text_attribute="word"/>
-    </p>
-    <p>
-      With Url and urlCallback
-      <auto-complete
-        url="https://api.datamuse.com/words?max=10&sp="
-        text_attribute="word"
-        :urlCallback="postProcess"
-      />
-    </p>
+    <h2>Autocomplete Example</h2>
+    <div class='event--notification'>
+        You chose: {{ chosenSuggestion }}
+    </div>
+    <div class='wrapper'>
+      <div class='example'>
+        <p>With Array</p>
+        <auto-complete
+          @autocomplete-selected="setChoice"
+          @autocomplete-clear="setChoice"
+          :dataArray="horses"/>
+      </div>
+      <div class='example'>
+        <p>With Method and custom noResultsText</p>
+        <auto-complete
+          :method="getHorses"
+          noResultsText="Where's the beef?"/>
+      </div>
+      <div class='example'>
+        <p>With Url</p>
+        <auto-complete
+          :url="endpoint"
+          textAttribute="word"/>
+      </div>
+      <div class='example'>
+        <p>With getUrl and urlCallback</p>
+        <auto-complete
+          :getUrl="constructUrl"
+          :urlCallback="postProcess"
+          textAttribute="word"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,10 +59,20 @@ export default {
           text: 'tall',
           abbr: 't'
         }
-      ]
+      ],
+      endpoint: "https://api.datamuse.com/words?sp=",
+      chosenSuggestion: null,
     }
   },
   methods: {
+    setChoice (value) {
+      if (!value) {
+        this.chosenSuggestion = null;
+        return
+      }
+      if (value.word) this.chosenSuggestion = value.word;
+      else if (value.text) this.chosenSuggestion = value.text;
+    },
     getHorses (query) {
       let horses =  [
         {
@@ -69,6 +89,9 @@ export default {
         }
       ];
       return horses.filter(h => h.text.indexOf(query) > -1)
+    },
+    constructUrl (query) {
+      return `${this.endpoint}${query}&max=4`
     },
     postProcess (data) {
       return data.map(d => {
@@ -89,9 +112,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-p {
- display: inline-block;
-  margin-right: 30px;
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 4%;
+  margin: 2% 10%;
 }
+.example {
+  display: inline-block;
+  margin-right: 30px;
+  height: 100vh;
+}
+
 
 </style>
--- src/components/AutoComplete.vue
+++ src/components/AutoComplete.vue
@@ -2,20 +2,28 @@
   <div class='autocomplete--container'>
     <input class='search-box' type='text'
       @input="getData(search_param)" v-model="search_param"/>
-    <div class='autocomplete--suggestions--container'>
-      <ul v-show="suggestions.length && search_param && !selected"
-          class='autocomplete--suggestions--list'>
+    <div class='autocomplete--suggestions--container'
+      v-show="suggestions.length && search_param && !selected">
+      <ul class='autocomplete--suggestions--list'>
         <li v-for="suggestion in suggestions">
-          <div @click="setParam(suggestion)"
-               class='suggestion'>
-            <span v-for='attribute in suggestion'>
+          <div
+              @click="setInputValue(suggestion)"
+              class='suggestion'>
+            <div
+              v-for='attribute in suggestion'
+              class='suggestion--attribute'>
               {{ attribute }}
-            </span>
+            </div>
           </div>
         </li>
       </ul>
-      <ul v-show="!suggestions.length && search_param">
-        <li>Sorry, no results</li>
+    </div>
+    <div class='autocomplete--suggestions--container'
+         v-show="!suggestions.length && search_param">
+      <ul class="no-results-container">
+        <li class="no-results-container">
+          {{ noResultsText}}
+        </li>
       </ul>
     </div>
   </div>
@@ -28,9 +36,11 @@ export default {
   props: {
     'method': Function,
     'url': String,
+    'getUrl': Function,
     'urlCallback': Function, // post process url
     'dataArray': Array,
-    'text_attribute': { type: String, default: 'text'}
+    'textAttribute': { type: String, default: 'text'},
+    'noResultsText': { type: String, default: 'Sorry, no results'}
   },
   data () {
     return {
@@ -40,19 +50,27 @@ export default {
     }
   },
   methods: {
-    setParam (suggestion) {
-      this.search_param = suggestion[this.text_attribute];
+    setInputValue (suggestion) {
+      this.search_param = suggestion[this.textAttribute];
       this.selected = true;
+      this.$emit('autocomplete-selected', suggestion);
     },
-    filterDataArray (query, things, text_attribute) {
-      return things.filter(t => t[text_attribute].indexOf(query) > -1)
+    filterDataArray (query, things, textAttribute) {
+      return things.filter(t => t[textAttribute].indexOf(query) > -1)
     },
     getData (query) {
       this.selected = false;
-      if (this.url) {
+      this.$emit('autocomplete-clear')
+      let url;
+      if (this.url || this.getUrl) {
+        if (this.getUrl) {
+          url = this.getUrl(query);
+        }
+        else {
+          url = `${this.url}${query}`;
+        }
         // TODO: throttle
         if (query.length >= 3) {
-          let url = `${this.url}${query}`;
           fetch(url).then(r => {
               return r.json()
             }).then((data)=> {
@@ -75,16 +93,44 @@ export default {
       }
       else if (this.dataArray) {
         this.suggestions = this.filterDataArray(
-          query, this.dataArray, this.text_attribute
+          query, this.dataArray, this.textAttribute
         );
       }
       else {
         throw new Error(
-          "You must provide callback or url or dataArray prop")
+          "You must provide callback, url, getUrl, or dataArray prop")
       }
     }
   },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+input {
+  width: 99%;
+}
+.autocomplete--suggestions--container {
+  border: 1px solid #ccc;
+  width: 100%;
+  border-top: none;
+}
+ul  {
+  margin: 0.5em;
+  margin-top: -1px;
+  padding: 0;
+  padding-top: 0.5em;
+  text-align: left;
+}
+li {
+  list-style-type: none;
+}
+.suggestion {
+  cursor: pointer;
+}
+.suggestion--attribute {
+  display: inline-block;
+  margin-right: 1em;
+  padding: 0.1em 0;
+}
+
+</style>
